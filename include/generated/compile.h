86c10b1715c37f569286a37d2e02220f23654c51
GenTarkin <amdking@hotmail.com>
2014-09-29 08:06:20 -0700

    added base files

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 10 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#10 SMP PREEMPT Sun Sep 28 15:22:11 PDT 2014"
+#define LINUX_COMPILE_BY "jason"
+#define LINUX_COMPILE_HOST "jason-androidkerneldev"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.8-scorched-ics-gd8289ff-dirty"
